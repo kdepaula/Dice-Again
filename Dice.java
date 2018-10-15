fdded174f788c2a4518c2123034eefe0e3d2db0a
kdepaula <42978472+kdepaula@users.noreply.github.com>
2018-10-15 14:11:40 -0400

    Add files via upload

--- Dice.java
+++ Dice.java
@@ -1,4 +1,34 @@
+/**
+ * 
+ * @author kdepaula20
+ *
+ */
+public class Dice 
+{
 
-public class Dice {
-
+		private int numRolls;
+		
+		public Dice()
+		{
+			numRolls = 0;
+		}
+		
+		public void reset()
+		{
+			numRolls = 0;
+		}
+		
+		public int roll()
+		{
+			numRolls++;
+			return (int) ((Math.random() * 6) + 1);	
+		}
+		//roll should not be a field, but you can make it a variable under
+		//the method roll but you can do it without that
+		
+		public int getNumRolls()
+		{
+			return numRolls;
+		}
 }
+
--- DiceSum.java
+++ DiceSum.java
@@ -1,11 +1,24 @@
 
-public class DiceSum {
+public class DiceSum 
+{
 
 	public static void main(String[] args) 
 	{
-		int totalRoll = 0;
-		Dice dice = new Dice();
-		Dice dice2 = new Dice();
+		Dice die1 = new Dice();
+		Dice die2 = new Dice();
+		int roll1 = die1.roll();
+		int roll2 = die2.roll();
+		System.out.println("Die 1: " + roll1 + "\tDie 2: " + roll2);
+		int totalRoll = 1;
+		while(roll1 != roll2)
+		{
+			roll1 = die1.roll();
+			roll2 = die2.roll();
+			System.out.println("Die 1: " + roll1 + "\tDie 2: " + roll2);
+			totalRoll++;
+		}
+		
+		System.out.println("The dice were rolled " + totalRoll + " times");
 	}
 
 }
